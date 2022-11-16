5666737685988b06bfe6056aa8b1d2f6e833a4db
mitchell1892 <mitchellkemal21@gmail.com>
2022-11-16 10:08:46 +0000

    upload

---
+++ Assets/CharaterController.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharaterController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    float maxSpeed = 3.0f;
+    float rotation = 0.0f;
+
+    void Update()
+    {
+       transform.position = transform.position + (transform.forward * Input.GetAxis("Vertical"));
+
+        rotation = rotation + Input.GetAxis("Mouse x");
+        transform.rotation = Quaternion.Euler(new vector3(0.0f,rotation, 0.0f));
+    }
+}
+
+
