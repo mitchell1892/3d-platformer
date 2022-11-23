ed81d9f3cffdd7ade2f1f48db64fe9b9a001e082
mitchell1892 <mitchellkemal21@gmail.com>
2022-11-23 10:40:50 +0000

    code

--- Assets/CharaterController.cs
+++ Assets/CharaterController.cs
@@ -4,23 +4,31 @@ using UnityEngine;
 
 public class CharaterController : MonoBehaviour
 {
+    
+    public float maxSpeed = 3.0f;
+    float rotation = 0.0f;
+    float camRotation = 0.0f;
+    GameObject cam;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        cam = GameObject.Find("Main Camera");
     }
 
-    // Update is called once per frame
-    float maxSpeed = 3.0f;
-    float rotation = 0.0f;
+    
+    
 
     void Update()
     {
-       transform.position = transform.position + (transform.forward * Input.GetAxis("Vertical"));
+       transform.position = transform.position + (transform.forward * Input.GetAxis("Vertical") * maxSpeed) + (transform.right * Input.GetAxis("Horizontal") * maxSpeed);
+
 
         rotation = rotation + Input.GetAxis("Mouse x");
         transform.rotation = Quaternion.Euler(new Vector3(0.0f,rotation, 0.0f));
-    }
-}
+        
+        camRotation = camRotation + Input.GetAxis("Mouse Y");
+        cam.transform.localRotation = Quaternion.Euler(new Vector3(camRotation, 0.0f, 0.0f));
+    }}
 
 
