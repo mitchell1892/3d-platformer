6b111605bed7fab0b642cd04e4e78191bed1d609
mitchell1892 <mitchellkemal21@gmail.com>
2022-11-24 15:32:24 +0000

    code

--- Assets/CharaterController.cs
+++ Assets/CharaterController.cs
@@ -9,11 +9,20 @@ public class CharaterController : MonoBehaviour
     float rotation = 0.0f;
     float camRotation = 0.0f;
     GameObject cam;
+    Rigidbody myRigidbody;
+
+    bool isOnGround;
+    public GameObject groundChecker;
+    public LayerMask groundLayer;
+
 
     // Start is called before the first frame update
     void Start()
     {
+        Cursor.lockState = CursorLockMode.Locked;
+
         cam = GameObject.Find("Main Camera");
+        myRigidbody = GetComponent<Rigidbody>();
     }
 
     
@@ -21,8 +30,10 @@ public class CharaterController : MonoBehaviour
 
     void Update()
     {
-       transform.position = transform.position + (transform.forward * Input.GetAxis("Vertical") * maxSpeed) + (transform.right * Input.GetAxis("Horizontal") * maxSpeed);
+       isOnGround = Physics.CheckSphere(groundChecker.transform.position, 0.1f, groundLayer);
 
+       Vector3 newVelocity = (transform.forward * Input.GetAxis("Vertical") * maxSpeed)  + (transform.right * Input.GetAxis("Horizontal") * maxSpeed);
+       myRigidbody.velocity = new Vector3(newVelocity.x, newVelocity.y, newVelocity.z);
 
         rotation = rotation + Input.GetAxis("Mouse x");
         transform.rotation = Quaternion.Euler(new Vector3(0.0f,rotation, 0.0f));
