2f8f94d6294184aae7a513f799124456c778c85b
mitchell1892 <mitchellkemal21@gmail.com>
2022-12-09 12:42:43 +0000

    character animation

--- Assets/CharaterController.cs
+++ Assets/CharaterController.cs
@@ -6,14 +6,16 @@ public class CharaterController : MonoBehaviour
 {
     
     public float maxSpeed = 3.0f;
-    float rotation = 0.0f;
-    float camRotation = 0.0f;
+    public float rotation = 0.0f;
+    public float camRotation = 0.0f;
     GameObject cam;
-
+    Rigidbody myRigidbody;
+    public Animator  myAnim;
     // Start is called before the first frame update
     void Start()
     {
         cam = GameObject.Find("Main Camera");
+        myRigidbody = GetComponent<Rigidbody>();
     }
 
     
@@ -21,14 +23,20 @@ public class CharaterController : MonoBehaviour
 
     void Update()
     {
-       transform.position = transform.position + (transform.forward * Input.GetAxis("Vertical") * maxSpeed) + (transform.right * Input.GetAxis("Horizontal") * maxSpeed);
+       
 
+       Vector3 newVelocity = transform.forward * Input.GetAxis("Vertical") * maxSpeed;
+       myRigidbody.velocity = new Vector3(newVelocity.x, myRigidbody.velocity.y, newVelocity.z);
 
-        rotation = rotation + Input.GetAxis("Mouse x");
+       myAnim.SetFloat("speed" , newVelocity.magnitude);
+
+        rotation = rotation + Input.GetAxis("Mouse X");
         transform.rotation = Quaternion.Euler(new Vector3(0.0f,rotation, 0.0f));
         
         camRotation = camRotation + Input.GetAxis("Mouse Y");
         cam.transform.localRotation = Quaternion.Euler(new Vector3(camRotation, 0.0f, 0.0f));
-    }}
+    }
+    
+}
 
 
